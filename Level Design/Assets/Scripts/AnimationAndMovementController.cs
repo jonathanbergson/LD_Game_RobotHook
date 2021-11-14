ff600fc6496dd2ccbed0689338ab63ff3c95e5f8
Jonathan Bergson <contato@bergson.me>
2021-11-14 19:19:45 -0300

    chore: pulo simples implementado

---
+++ Level Design/Assets/Scripts/AnimationAndMovementController.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class AnimationAndMovementController : MonoBehaviour
+{
+    CharacterController characterController;
+
+    // variables to store player input values
+    Vector3 currentMovement;
+
+    // gravity variables
+    float gravity = -9.8f;
+    float groundedGravity = -0.05f;
+
+    // jump variables
+    bool isJumpPressed = false;
+    float initialJumpVelocity;
+    float maxJumpHeight = 2.0f;
+    float maxJumpTime = 0.5f;
+    bool isJumping = false;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        SetupJumpVariables();
+    }
+
+    void Update()
+    {
+        OnJump();
+        characterController.Move(currentMovement * Time.deltaTime);
+        HandleGravity();
+        HandleJump();
+    }
+
+    void HandleGravity()
+    {
+        if (characterController.isGrounded) {
+            currentMovement.y = groundedGravity;
+        } else {
+            currentMovement.y += gravity * Time.deltaTime;
+        }
+    }
+
+    void SetupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+    }
+
+    void OnJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isJumpPressed = true;
+        } else {
+            isJumpPressed = false;
+        }
+    }
+
+    void HandleJump()
+    {
+        Debug.Log(characterController.isGrounded);
+        if (!isJumping && characterController.isGrounded && isJumpPressed) {
+            isJumping = true;
+            currentMovement.y = initialJumpVelocity;
+        } else if (isJumping && characterController.isGrounded && !isJumpPressed) {
+            isJumping = false;
+        }
+    }
+}
