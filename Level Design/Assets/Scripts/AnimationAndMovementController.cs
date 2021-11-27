e4e01b0f6c960f74dcbc0cb863a5fff69b52be51
Jonathan Bergson <contato@bergson.me>
2021-11-26 21:02:41 -0300

    feat: implementado gancho no game

--- Level Design/Assets/Scripts/AnimationAndMovementController.cs
+++ Level Design/Assets/Scripts/AnimationAndMovementController.cs
@@ -2,27 +2,34 @@ using UnityEngine;
 
 public class AnimationAndMovementController : MonoBehaviour
 {
+    public Transform jumpPoint;
+
+    // personagem
     public Animator animator;
     CharacterController characterController;
 
-    // variables to store player input values
+    // controlar deslocamento
     Vector3 currentMovement;
 
-    // gravity variables
+    // grávidade
     float gravity = -9.8f;
     float groundedGravity = -0.05f;
 
-    // movemente variables
+    // movimento
     bool isRunning = false;
-    int isRunningPressed = 0;
+    float runningDirection = 1;
     float runningVelocity = 6.0f;
 
-    // jump variables
+    // salto
     bool isJumping = false;
     bool isJumpPressed = false;
     float initialJumpVelocity;
     float maxJumpHeight = 4.0f;
     float maxJumpTime = 0.6f;
+
+    // usar gancho
+    bool isUseHook = false;
+    float shiftHookVelocity = 2.0f;
 
     void Awake()
     {
@@ -32,11 +39,58 @@ public class AnimationAndMovementController : MonoBehaviour
 
     void Update()
     {
-        OnMovement();
+        OnRun();
         OnJump();
+        OnUseHook();
         characterController.Move(currentMovement * Time.deltaTime);
         HandleGravity();
+        HandleMovement();
         HandleJump();
+    }
+    void SetupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+    }
+
+    void OnRun()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetBool("isRunning", true);
+            isRunning = true;
+            runningDirection = - 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isRunning", true);
+            isRunning = true;
+            runningDirection = 1;
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+            isRunning = false;
+        }
+    }
+
+    void OnJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isJumpPressed = true;
+        } else {
+            isJumpPressed = false;
+        }
+    }
+
+    void OnUseHook()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            isUseHook = true;
+        } else {
+            isUseHook = false;
+        }
     }
 
     void HandleGravity()
@@ -49,37 +103,36 @@ public class AnimationAndMovementController : MonoBehaviour
         }
     }
 
-    void OnMovement()
+    void HandleMovement()
     {
-        if (Input.GetKey(KeyCode.A)) {
-            animator.SetBool("isRunning", true);
-            currentMovement.z = runningVelocity * -1;
-            Vector3 look = Vector3.left - transform.position;
-            transform.rotation =  Quaternion.LookRotation(Vector3.back, Vector3.up);
-        } else if (Input.GetKey(KeyCode.D)) {
-            animator.SetBool("isRunning", true);
-            currentMovement.z = runningVelocity * 1;
-            Vector3 look = Vector3.right - transform.position;
-            transform.rotation =  Quaternion.LookRotation(Vector3.forward, Vector3.up);
-        } else {
-            animator.SetBool("isRunning", false);
-            currentMovement.z = runningVelocity * 0;
+        if (isUseHook)
+        {
+            Vector3 dir = jumpPoint.position - transform.position;
+            currentMovement = dir.normalized * initialJumpVelocity * 1.4f;
         }
-    }
 
-    void SetupJumpVariables()
-    {
-        float timeToApex = maxJumpTime / 2;
-        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex, 2);
-        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
-    }
 
-    void OnJump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            isJumpPressed = true;
-        } else {
-            isJumpPressed = false;
+        if (characterController.isGrounded && !isUseHook)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                animator.SetBool("isRunning", true);
+                currentMovement.z = runningVelocity * -1;
+                Vector3 look = Vector3.left - transform.position;
+                transform.rotation = Quaternion.LookRotation(Vector3.back, Vector3.up);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                animator.SetBool("isRunning", true);
+                currentMovement.z = runningVelocity * 1;
+                Vector3 look = Vector3.right - transform.position;
+                transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+            }
+            else
+            {
+                animator.SetBool("isRunning", false);
+                currentMovement.z = runningVelocity * 0;
+            }
         }
     }
 
