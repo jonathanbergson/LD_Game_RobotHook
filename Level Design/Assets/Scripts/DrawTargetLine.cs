0deb93f6ec94d71ff74ed228c32d828e24f63594
Jonathan Bergson <contato@bergson.me>
2021-11-26 21:03:01 -0300

    refactor: removido Debug.Log

--- Level Design/Assets/Scripts/DrawTargetLine.cs
+++ Level Design/Assets/Scripts/DrawTargetLine.cs
@@ -21,34 +21,5 @@ public class DrawTargetLine : MonoBehaviour
 
         targetLine.SetPosition(0, origin);
         targetLine.SetPosition(1, origin + (target - origin).normalized * 5);
-
-        if (Input.GetMouseButtonUp(0))
-        {
-
-            Debug.Log("LOG");
-            Debug.Log(origin);
-            Debug.Log(target);
-            Debug.Log((target - origin));
-            Debug.Log((target - origin).normalized);
-
-            //    Debug.Log("LOG");
-            //    Debug.Log(Input.mousePosition);
-            //    Debug.Log(transform.position);
-            //    Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.x)));
-
-            //    Vector3 mousePos;
-            //    mousePos = Input.mousePosition;
-            //    Debug.Log("C1: " + mousePos.ToString());
-            //    mousePos.z = 10;
-            //    mousePos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 1.0f));
-            //    Debug.Log("C2: " + mousePos.ToString());
-
-            //    Vector3 mouseWzPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z);
-            //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(mouseWzPos);
-
-            //    mousePos.z = 0;
-            //    Debug.Log("C2: " + mousePos.ToString());
-
-        }
     }
 }
